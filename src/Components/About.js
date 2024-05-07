962147c70052a3e47c6735969d98e59084b9fb0a
Alina Prystynska <pristinskayaalina9@gmail.com>
2024-05-07 13:40:02 +0100

    07/05

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Container } from "react-bootstrap";
 import Image from "react-bootstrap/Image";
+import Figure from "react-bootstrap/Figure";
 import about1 from "./images/about1.webp";
 import banner from "./images/banner.jpg";
 import TotalPrice from "./TotalPrice";
@@ -15,13 +16,22 @@ function About() {
       </div>
       {/* The banner image has been moved to a container to ensure consistency */}
       <Container>
-      <Image src={banner} fluid />
-        <h3>Store Description</h3>
-        <h4>
-          online store - curtains and mats in the bathroom, tablecloths, mops
-          and much more
-        </h4>
-        <Image src={about1} fluid />
+        <Figure style={{ display: "flex"}}>
+          <Figure.Image
+            width={171}
+            height={180}
+            alt="171x180"
+            src={about1}
+            fluid
+          />
+          <Figure.Caption>
+            <h5>
+              Store Description: online store - curtains and mats in the
+              bathroom, tablecloths, mops and much more
+            </h5>
+          </Figure.Caption>
+        </Figure>
+        <Image src={banner} fluid/>
         <p>Contact Us at: contact@store.com</p>
       </Container>
     </div>
